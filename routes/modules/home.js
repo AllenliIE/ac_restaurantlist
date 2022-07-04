d0e3a95b0c23ce7d7a56d006950e3b2f8fcfc92d
AllenliIE <allen.li.service@gmail.com>
2022-07-04 11:36:55 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,8 +5,10 @@ const Restaurant = require('../../models/restaurant')
 
 //setting Schema to render index
 router.get('/', (req, res) => {
+  const sorting = req.query
   Restaurant.find()
     .lean()
+    .sort({ _id: 'asc' }) //A~Z
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
---
+++ routes/modules/sort.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant')
+
+router.get('/nameAse', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'asc' })
+    .then(restaurants => {
+      res.render('index', { restaurants })
+    })
+    .catch(error => { console.log(error) })
+})
+
+router.get('/nameDesc', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'desc' })
+    .then(restaurants => {
+      res.render('index', { restaurants })
+    })
+    .catch(error => { console.log(error) })
+})
+
+module.exports = router
