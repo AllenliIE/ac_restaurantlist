2dea893049d04c742e634f05228e712da68c3be8
AllenliIE <allen.li.service@gmail.com>
2022-07-05 19:07:17 +0800

    feat: add sort function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -68,5 +68,4 @@ router.delete('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
 module.exports = router
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,15 +4,15 @@ const Restaurant = require('../../models/restaurant')
 
 //setting search function
 router.get('/', (req, res) => {
-  const keywords = req.query.keyword
+  const keywords = req.query.keyword?.trim()
+  const regExp = new RegExp(keywords, 'i')
 
-  return Restaurant.find()
+  Restaurant.find({
+    $or: [{ name: regExp }, { category: regExp }]
+  })
     .lean()
-    .then(Restaurant => {
-      const restaurants = Restaurant.filter(list => {
-        return list.name.toLowerCase().includes(keywords.toLowerCase()) ||
-          list.category.toLowerCase().includes(keywords.toLowerCase())
-      })
+    .sort({ _id: 'asc' })
+    .then(restaurants => {
       res.render('index', { restaurants, keywords })
     })
     .catch(error => console.error(error))
