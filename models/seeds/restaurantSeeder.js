8e3395152755c34b8cd996a5dd3a95f8f131ed73
AllenliIE <allen.li.service@gmail.com>
2022-08-28 03:34:14 +0800

    feat: modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,50 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant')
-const restaurantList = require('../../restaurants.json').results
+const restaurantList = require('./restaurants.json').results
+const User = require('../user')
 const db = require('../../config/mongoose')
+const SEED_USERS = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}, {
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}]
 
 db.once('open', () => {
-  for (let i = 0; i < restaurantList.length; i++) {
-    Restaurant.create({
-      ...restaurantList[i]
-    })
-  }
+  SEED_USERS.map(SEED_USER => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER.password, salt))
+      .then(hash => User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+      ).then(user => {
+        if (user.email === 'user1@example.com') {
+          start = 0
+        } else if (user.email === 'user2@example.com') {
+          start = 3
+        }
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, i) => {
+            i += start
+            restaurantList[i].userId = user._id
+            return Restaurant.create(restaurantList[i])
+          }))
+      }).then(() => {
+
+        console.log('usersAdmin done.')
+        process.exit()
+      })
+
+    console.log('done')
+  })
 })
