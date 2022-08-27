7f9995f096dc1b7d2fadfdeff90d214049efad4c
AllenliIE <allen.li.service@gmail.com>
2022-08-27 11:55:21 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const sort = require('./modules/sort')
 const search = require('./modules/search')
+const users = require('./modules/users') //add users source
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/sort', sort)
 router.use('/search', search)
+router.use('/users', users) //add users router
 
 module.exports = router
